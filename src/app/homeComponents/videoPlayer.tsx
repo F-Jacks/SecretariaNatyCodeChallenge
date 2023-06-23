2504bfdaa3dd59a7d58afb782587a99bc3040d48
F-Jacks <131964664+F-Jacks@users.noreply.github.com>
2023-06-23 17:45:50 +0000

    po

--- src/app/homeComponents/videoPlayer.tsx
+++ src/app/homeComponents/videoPlayer.tsx
@@ -2,7 +2,7 @@ import { src } from "@/mocks/home/videoPlayer";
 
 const VideoPlayer = () => {
     return (
-        <section className="aspect-w-16 aspect-h-9 w-full bg-white">
+        <section className="aspect-w-16 aspect-h-9 w-full">
             <iframe src={src} allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
         </section>
     );
