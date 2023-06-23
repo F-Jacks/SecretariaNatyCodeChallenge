003fef55cb7906abaeab9c9002893af51b75cd31
F-Jacks <131964664+F-Jacks@users.noreply.github.com>
2023-06-23 20:16:22 +0000

    ew

--- src/app/sign/page.tsx
+++ src/app/sign/page.tsx
@@ -17,14 +17,14 @@ const Sign = (props: Props) => {
                     alt={""}
                     width={1200}
                     height={1200}
-                    className='absolute top-0 right-0 w-1/2 h-full'
+                    className='absolute top-0 right-0 w-1/2 h-full bg-white -z-10'
                 />
-                <h1>
-                    {"oi"}
+                <h1 className='bg-black absolute top-1/2 -tranalte-y-1/2 right-[25%] text-white w-3/4 max-w-2xl'>
+                    {"Work with us"}
                 </h1>
-                <h2>
-                    {"hj"}
-                </h2>
+                {/* <h2 className='bg-black text-white w-3/4 max-w-2xl'>
+                    {"wkhqoieu ewoifypqeiwo eiowryoiewu hapiufwe fiufewhoreiuqheirq fpiuhfpoiuew difpiuewbfpi ofihpewiurwi fdpahweipoufweq dçpafigfhoweiu pdfiuhpoweiuqb sdiuehwrpiuoew daoifhpewqiure"}
+                </h2> */}
             </section>
             <section className='flex flex-col gap-y-[1.6rem] lt:flex-row gap-x-8 px-[5%] mx-auto w-full max-w-6xl'>
                 <LinkButton href={'/'}>
