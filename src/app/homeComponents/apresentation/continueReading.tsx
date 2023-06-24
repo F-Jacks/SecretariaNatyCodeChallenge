7d935e46f5155575ba1fdbd8a1afba8053980bc4
F-Jacks2 <137643571+F-Jacks2@users.noreply.github.com>
2023-06-24 19:21:11 +0000

    rt

--- src/app/homeComponents/apresentation/continueReading.tsx
+++ src/app/homeComponents/apresentation/continueReading.tsx
@@ -1,56 +1,10 @@
-"use client"
-
-
 import { continueReadingIcon, continueReadingText } from "@/mocks/home/apresentation";
-import startRidingFloatLinkAtom from "@/states/startRidingFloatLink";
-import { useLayoutEffect, useRef, useState } from "react";
-import { useSetRecoilState } from "recoil";
 
 const ContinueReading = () => {
-    const componenteRef = useRef<HTMLDivElement>(null);
-    const setStartRidingFloatLink = useSetRecoilState(startRidingFloatLinkAtom);
-    const [isBottom, setIsBottom] = useState(false);
-
-    useLayoutEffect(() => {
-        const observer = new IntersectionObserver((entries) => {
-          entries.forEach((entry) => {
-            if (!entry.isIntersecting && isBottom) {
-              setStartRidingFloatLink({
-                start: true,
-                end: false
-              });
-            } else {
-              setStartRidingFloatLink({
-                start: false,
-                end: false
-              });
-            }
-          });
-        });
-      
-        const handleScroll = () => {
-          if (componenteRef.current) {
-            const componentePosicao = componenteRef.current.getBoundingClientRect().bottom;
-            setIsBottom(componentePosicao <= 0);
-          }
-        };
-      
-        if (componenteRef.current) {
-          observer.observe(componenteRef.current);
-          window.addEventListener('scroll', handleScroll);
-        }
-      
-        return () => {
-          window.removeEventListener('scroll', handleScroll);
-          observer.disconnect();
-        };
-    }, [isBottom]);
-
-
     return (
         <div 
             className="w-1/2 gap-y-[0.8rem] max-w-2xl min-w-[10rem] flex flex-col items-center row-start-4 cursor-pointer"
-            ref={componenteRef}
+            id={"continueReadingDiv"}
         >
             <span className="text-sm text-white">
                 {continueReadingText}
--- src/app/homeComponents/price.tsx
+++ src/app/homeComponents/price.tsx
@@ -1,62 +1,12 @@
-"use client"
-
-
 import { amount, text, title, link } from "@/mocks/home/price";
 import LinkButton from "../components/linkButton";
-import { useLayoutEffect, useRef, useState } from "react";
-import { useRecoilState } from "recoil";
-import startRidingFloatLinkAtom from "@/states/startRidingFloatLink";
 
 
 const Price = () => {
-    const componenteRef = useRef<HTMLDivElement>(null);
-    const [startRidingFloatLink, setStartRidingFloatLink] = useRecoilState(startRidingFloatLinkAtom);
-    const [isBottom, setIsBottom] = useState(false);
-
-    useLayoutEffect(() => {
-      let isComponentUnmounted = false; // Variável de controle
-    
-      const observer = new IntersectionObserver((entries) => {
-        if (!isComponentUnmounted && startRidingFloatLink.start) {
-          entries.forEach((entry) => {
-            if (!entry.isIntersecting && !isBottom) {
-              setStartRidingFloatLink((prevStartRidingFloatLink) => ({
-                ...prevStartRidingFloatLink,
-                end: false
-              }));
-            } else {
-              setStartRidingFloatLink((prevStartRidingFloatLink) => ({
-                ...prevStartRidingFloatLink,
-                end: true
-              }));
-            }
-          });
-        }
-      });
-    
-      const handleScroll = () => {
-        if (!isComponentUnmounted && componenteRef.current) {
-          const componentePosicao = componenteRef.current.getBoundingClientRect().bottom;
-          setIsBottom(componentePosicao <= 0);
-        }
-      };
-    
-      if (componenteRef.current) {
-        observer.observe(componenteRef.current);
-        window.addEventListener('scroll', handleScroll);
-      }
-    
-      return () => {
-        isComponentUnmounted = true; // Marcar o componente como desmontado
-        window.removeEventListener('scroll', handleScroll);
-        observer.disconnect();
-      };
-    }, [isBottom, startRidingFloatLink]);
-
     return (
         <section 
             className="w-fit mx-auto px-[5%]"
-            ref={componenteRef}
+            id={"priceSection"}
         >
             <div className="p-4 border">
                 <div className="flex mb-12 gap-x-8 gap-y-4 mx-auto flex-col-reverse justify-center items-center lt:flex-row">
