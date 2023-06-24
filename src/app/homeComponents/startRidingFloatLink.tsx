78b6a090704da319c010ee0cb468810dad0ee96b
F-Jacks2 <137643571+F-Jacks2@users.noreply.github.com>
2023-06-24 19:54:27 +0000

    Finishing Start Float link

--- src/app/homeComponents/startRidingFloatLink.tsx
+++ src/app/homeComponents/startRidingFloatLink.tsx
@@ -1,22 +1,39 @@
 "use client"
 
-
-import startRidingFloatLinkAtom from "@/states/startRidingFloatLink";
-import { useRecoilValue } from "recoil";
 import LinkButton from "../components/linkButton";
 import { clientLink } from "@/mocks/home/apresentation";
 import { title } from "@/mocks/home/price";
 import classNames from "classnames";
+import { useLayoutEffect, useState } from "react";
 
 
 const StartRidingFloatLink = () => {
-    const startRidingFloatLink = useRecoilValue(startRidingFloatLinkAtom);
+    const [active, setActive] = useState(false);
+
+    useLayoutEffect(() => {
+      const bottomElement = document.getElementById("priceSection");
+      const topElement = document.getElementById("continueReadingDiv");
+
+      if (!topElement || !bottomElement) return;
+
+      const handleScroll = () => {
+        const onBottom = bottomElement.getBoundingClientRect().top - window.innerHeight;
+        console.log(onBottom, topElement.getBoundingClientRect().bottom);
+        setActive(topElement.getBoundingClientRect().bottom <= 0 && onBottom > 0);
+      };
+
+      window.addEventListener('scroll', handleScroll);
+
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
 
     return (
       <div className={classNames({
         ["fixed bottom-0 w-full h-fit transform transition-transform duration-250"]: true,
-        ["translate-y-0"]: !startRidingFloatLink.end && startRidingFloatLink.start,
-        ["translate-y-[100%]"]: startRidingFloatLink.end || !startRidingFloatLink.start
+        ["translate-y-0"]: active,
+        ["translate-y-[100%]"]: !active
       })}>
         <LinkButton href={clientLink.link} className="w-full" isFilled>
           {title} 
